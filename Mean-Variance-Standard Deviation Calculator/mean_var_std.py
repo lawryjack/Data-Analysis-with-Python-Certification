bc08f5afa5529f36e52d3f8586f6c7caed9248b0
lawryjack <91913018+lawryjack@users.noreply.github.com>
2022-05-04 19:09:04 -0600

    Add files via upload

---
+++ Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def calculate(list1):
+
+    #Error handling (list must be 9 elements)
+    if len(list1) != 9:
+        raise ValueError("List must contain nine numbers.");
+
+    x = np.array(list1);
+    array1 = np.split(x, 3);
+    #print(type(calculations));
+    array1 = np.array(array1);
+    #print(type(calculations));
+
+    #Calculating all math functions
+
+    #Mean
+    meanTotal = [list(np.mean(array1, axis=0)), list(np.mean(array1, axis=1)), np.mean(array1)];
+
+    #Variance
+    varTotal = [list(np.var(array1, axis=0)), list(np.var(array1, axis=1)), np.var(array1)];
+
+    #Standard Deviation
+    stdTotal = [list(np.std(array1, axis=0)), list(np.std(array1, axis=1)), np.std(array1)];
+
+    #Max
+    maxTotal = [list(np.max(array1, axis=0)), list(np.max(array1, axis=1)), np.max(array1)];
+
+    #Min
+    minTotal = [list(np.min(array1, axis=0)), list(np.min(array1, axis=1)), np.min(array1)];
+
+    #Sum
+    sumTotal = [list(np.sum(array1, axis=0)), list(np.sum(array1, axis=1)), np.sum(array1)];
+
+    calculations = {"mean": meanTotal, "variance": varTotal, "standard deviation": stdTotal, "max": maxTotal, "min": minTotal, "sum": sumTotal};
+
+    return calculations;
